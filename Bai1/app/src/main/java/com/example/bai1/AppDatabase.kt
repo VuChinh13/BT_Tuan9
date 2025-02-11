f58697cd5112aebfd8bd98d0cd50c75e351296d2
VuChinh13 <vuvanchinh1382k4@gmail.com>
2025-02-11 10:25:55 +0700

    edit code

--- Bai1/app/src/main/java/com/example/bai1/AppDatabase.kt
+++ Bai1/app/src/main/java/com/example/bai1/AppDatabase.kt
@@ -1,33 +1,33 @@
-    package com.example.bai1
-    
-    import android.content.Context
-    import androidx.room.Database
-    import androidx.room.Room
-    import androidx.room.RoomDatabase
-    import kotlin.concurrent.Volatile
-    
-    // Chỉ định lớp là 1 cơ sở dữ liệu
-    @Database(entities = [Note::class], version = 1)
-    abstract class AppDatabase : RoomDatabase() {
-    
-        abstract fun noteDao(): NoteDao
-    
-        companion object {
-            @Volatile
-            private var INSTANCE: AppDatabase? = null
-    
-            fun getInstance(context: Context): AppDatabase {
-                return INSTANCE ?: synchronized(this) {
-                    val instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        AppDatabase::class.java,
-                        "app_database"
-                    )
-                        .fallbackToDestructiveMigrationFrom() // khi mà có sự thay đổi gì thì tạo lại dữ liệu
-                        .build()
-                    INSTANCE = instance
-                    instance
-                }
+package com.example.bai1
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlin.concurrent.Volatile
+
+// Chỉ định lớp là 1 cơ sở dữ liệu
+@Database(entities = [Note::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database.db"
+                )
+                    .fallbackToDestructiveMigrationFrom() // khi mà có sự thay đổi gì thì tạo lại dữ liệu
+                    .build()
+                INSTANCE = instance
+                instance
             }
         }
     }
+}
--- Bai1/app/src/main/java/com/example/bai1/ItemAdapter.kt
+++ Bai1/app/src/main/java/com/example/bai1/ItemAdapter.kt
@@ -86,5 +86,4 @@ class ItemAdapter(
             checkBox = binding.cbDelete
         }
     }
-
 }
